08456f4f88f3b60fc421e12d9b62a74c17660b07
ghdtjgus76 <ghdtjgus76@naver.com>
2023-09-25 11:42:43 +0900

    chore: husky 관련 설치 및 세팅

--- .vite/deps_temp_d7117c14/react.js
+++ .vite/deps_temp_d7117c14/react.js
@@ -1,5 +1,3 @@
-import {
-  require_react
-} from "./chunk-XQVR23J4.js";
-export default require_react();
+import { require_react } from './chunk-XQVR23J4.js'
+export default require_react()
 //# sourceMappingURL=react.js.map
