151745d2908f200f9d4b6941f9f4a5941b07f711
ghdtjgus76 <ghdtjgus76@naver.com>
2023-09-26 14:12:23 +0900

    fix: storybook dynamic import 에러 해결

--- .vite/deps_temp_d7117c14/react.js
+++
@@ -1,3 +0,0 @@
-import { require_react } from './chunk-XQVR23J4.js'
-export default require_react()
-//# sourceMappingURL=react.js.map
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const queryClient = new QueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div></div>
       <ReactQueryDevtools />
     </QueryClientProvider>
   )
