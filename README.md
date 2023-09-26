c2570013923c98c05493430706e3ea3c680e4ccf
ghdtjgus76 <ghdtjgus76@naver.com>
2023-09-26 13:33:54 +0900

    fix: storybook 에러 해결

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## 프로젝트 세팅 테스트
 
-[yarn berry + vite + react + typescript + react-query + emotion + prettier + eslint + husky + storybook 프로젝트 세팅을 해보자] https://gugu76.tistory.com/115
+[yarn berry + vite + react + typescript + react-query + emotion + prettier + eslint + husky + lint-staged + storybook 프로젝트 세팅을 해보자] https://gugu76.tistory.com/115
---
+++ storybook-static/assets/preview-9e23412c.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-d690f8f1.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-c3f5b2af.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-c3f5b2af.js","./iframe-d690f8f1.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./_getPrototype-ddf2eac9.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-9e23412c.js.map
