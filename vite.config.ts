12405e3da0c7fd673e48aa28566fffe442974faa
ghdtjgus76 <ghdtjgus76@naver.com>
2023-09-26 15:40:52 +0900

    chore: github pages 관련 배포 설정 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-setting/',
+  base: '/',
   publicDir: './public',
   cacheDir: './.vite',
-  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg'],
   resolve: {
     alias: [
       { find: '@pages', replacement: '/src/pages' },
