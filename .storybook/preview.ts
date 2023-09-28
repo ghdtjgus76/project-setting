c1a2de8525eda68f825125959c1b111016b2a497
ghdtjgus76 <ghdtjgus76@naver.com>
2023-09-28 23:11:13 +0900

    chore: 스토리북 뷰포트 및 기본 패딩 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,23 @@
-import type { Preview } from '@storybook/react'
+import { Preview } from '@storybook/react';
+
+const customViewports = {
+  iPhone13: {
+    name: 'iPhone 13',
+    styles: {
+      width: '390px',
+      height: '844px'
+    },
+    type: 'mobile'
+  },
+  tablet: {
+    name: 'iPad Pro 11',
+    styles: {
+      width: '834px',
+      height: '1194px',
+    },
+    type: 'tablet',
+  },
+}
 
 const preview: Preview = {
   parameters: {
@@ -9,7 +28,18 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
-  },
-}
+    paddings: {
+      values: [
+        { name: 'margin-template', value: '24px' },
+        { name: 'none', value: '0px' },
+      ],
+      default: 'none',
+    },
+    viewport: { 
+      viewports: customViewports, 
+      defaultViewport: 'iPhone13'
+    },
+  }
+};
 
-export default preview
+export default preview;
